3dac4de25580aca7bc8966eabf5e98f7bdc7d46d
Denis Fadeev <denis@fadeev.org>
2020-04-09 20:16:41 +0500

    init

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const axios = require("axios");
+const app = express();
+
+app.use(express.static("public"));
+
+app.get("/txs", async (request, response) => {
+  const domain = "http://goz.irisnet.org:26657";
+  const path = "/tx_search?query=%22tx.height%3E0%22";
+  const txs = (await axios.get(domain + path)).data.result.txs;
+  let data = [];
+  txs.forEach((tx) => {
+    tx.tx_result.events.forEach((event) => {
+      let attributes = {};
+      event.attributes.forEach((a) => {
+        const key = Buffer.from(a.key, "base64").toString("utf-8");
+        const valueRaw = Buffer.from(a.value, "base64").toString("utf-8");
+        let value;
+        try {
+          value = JSON.parse(valueRaw);
+        } catch {
+          value = valueRaw;
+        }
+        attributes[key] = value;
+      });
+      data.push({
+        height: tx.height,
+        tx_hash: tx.hash,
+        event_type: event.type,
+        ...attributes,
+      });
+    });
+  });
+  response.json(data);
+});
+
+const listener = app.listen(process.env.PORT, () => {
+  console.log("Your app is listening on port " + listener.address().port);
+});

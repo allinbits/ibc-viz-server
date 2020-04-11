870a9872356b97c71ae5b6c10a1f71a1df3fbe45
Denis Fadeev <denis@fadeev.org>
2020-04-12 01:19:02 +0500

    All transactions

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const ReconnectingWebSocket = require("reconnecting-websocket");
+const config = require("./config.json");
 
 app.use(express.static("public"));
 
@@ -34,29 +35,45 @@ app.get("/", async (request, response) => {
 });
 
 app.get("/txs", async (request, response) => {
-  const domain = "http://ibc.umbrellavalidator.com:26657";
-  const path = "/tx_search?query=%22tx.height%3E0%22";
-  const txs = (await axios.get(domain + path)).data.result.txs;
+  const path = "tx_search?query=%22tx.height%3E0%22";
   let data = [];
-  txs.forEach((tx) => {
-    tx.tx_result.events.forEach((event) => {
-      let attributes = {};
-      event.attributes.forEach((a) => {
-        const key = Buffer.from(a.key, "base64").toString("utf-8");
-        const valueRaw = Buffer.from(a.value, "base64").toString("utf-8");
-        let value;
+  const blockchains = (
+    await Promise.all(
+      config.blockchains.map(async (domain) => {
+        const url = `http://${domain}:26657/${path}`;
         try {
-          value = JSON.parse(valueRaw);
+          return {
+            name: domain,
+            txs: (await axios.get(url)).data.result.txs,
+          };
         } catch {
-          value = valueRaw;
+          console.log("Fetch failed.");
         }
-        attributes[key] = value;
-      });
-      data.push({
-        height: tx.height,
-        tx_hash: tx.hash,
-        event_type: event.type,
-        ...attributes,
+      })
+    )
+  ).filter((b) => b && b.txs);
+  blockchains.forEach((blockchain) => {
+    blockchain.txs.forEach((tx) => {
+      tx.tx_result.events.forEach((event) => {
+        let attributes = {};
+        event.attributes.forEach((a) => {
+          const key = Buffer.from(a.key, "base64").toString("utf-8");
+          const valueRaw = Buffer.from(a.value, "base64").toString("utf-8");
+          let value;
+          try {
+            value = JSON.parse(valueRaw);
+          } catch {
+            value = valueRaw;
+          }
+          attributes[key] = value;
+        });
+        data.push({
+          height: tx.height,
+          tx_hash: tx.hash,
+          event_type: event.type,
+          blockchain: blockchain.name,
+          ...attributes,
+        });
       });
     });
   });
